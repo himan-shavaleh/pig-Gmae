ecf8e19a736bd3262ac917eab2fd300ecd5eb74a
himan shavaleh <88880060+himan-shavaleh@users.noreply.github.com>
2021-10-17 23:09:42 +0330

    code refactored

--- dist/main.js
+++ dist/main.js
@@ -1,78 +1,75 @@
 // defining ui variables
 const newGame = document.querySelector('#new-game');
-const player1 = document.querySelector('#player-1');
-const player2 = document.querySelector('#player-2');
 const img = document.querySelector('#img');
 const rollDice = document.querySelector('#roll-dice');
 const hold = document.querySelector('#hold');
-const pl1Point = document.querySelector('#pl-1-point');
-const pl2Point = document.querySelector('#pl-2-point');
-const currentPl1 = document.querySelector('#current-pl1');
-const currentPl2 = document.querySelector('#current-pl2');
+
 
 //app variables
 const currentPlayer = document.querySelector('.active');
-let scorePl1 = 0;
-let scorePl2 = 0;
-let currentScorePl1 = 0;
-let currentScorePl2 = 0;
-let rand;
+let scores = [0,0];
+let activPlayer = 1;
+let currentScore = 0;
 
+
+init();
 // events
-newGame.addEventListener('click',setDefault);
+newGame.addEventListener('click', setDefault);
 rollDice.addEventListener('click', game);
 hold.addEventListener('click', holdFunc);
 
 // perduces the game
 function game() {
-
-    rand = Math.trunc((Math.random() * 6) + 1);
-
+   const rand = Math.trunc((Math.random() * 6) + 1);
+    img.classList.remove('hidden');
     img.src = `dice-${rand}.png`;
     if (rand === 1) {
-        if (player1.classList.contains('active')) {
-            player1.classList.remove('active');
-            player2.classList.add('active');
-            currentScorePl1 = 0;
-            currentPl1.innerHTML = 0;
-        } else if (player2.classList.contains('active')) {
-            player2.classList.remove('active');
-            player1.classList.add('active');
-            currentScorePl2 = 0;
-            currentPl2.innerHTML = 0;
-        }
+        changePlayer();
     } else {
-        if (player1.classList.contains('active')) {
-            currentScorePl1 += rand;
-            currentPl1.innerHTML = currentScorePl1;
-        } else if (player2.classList.contains('active')) {
-            currentScorePl2 += rand;
-            currentPl2.innerHTML = currentScorePl2;
-        }
+        
+        currentScore += rand;
+        document.querySelector(`#current-pl${activPlayer}`).innerHTML = currentScore;
+        
     }
 }
 
 // holds the scorse
 function holdFunc() {
-    if (player1.classList.contains('active')) {
-        scorePl1 += currentScorePl1;
-        pl1Point.innerHTML = scorePl1;
-        player1.classList.remove('active');
-            player2.classList.add('active');
-            currentScorePl1 = 0;
-            currentPl1.innerHTML = 0;
-    } else if (player2.classList.contains('active')) {
-        scorePl2 += currentScorePl2;
-        pl2Point.innerHTML = scorePl2;
-        player2.classList.remove('active');
-            player1.classList.add('active');
-            currentScorePl2 = 0;
-            currentPl2.innerHTML = 0;
+    
+    scores[activPlayer-1]+=currentScore;
+    document.getElementById(`pl-${activPlayer}-point`).innerHTML = scores[activPlayer-1];
+    
+    if(scores[activPlayer-1]>=100){
+        document.getElementById(`player-${activPlayer}`).style.background = '#22cc39';
+        document.querySelector('.message').innerHTML = `Player ${activPlayer} Win 🎈`;
     }
+    changePlayer();
 }
 
 
 // reloads everything
-function setDefault(){
-    location.reload();
+function setDefault() {
+init();
+}
+// change active player
+function changePlayer(){
+    currentScore = 0;
+    document.querySelector(`#current-pl${activPlayer}`).innerHTML = 0;
+    document.querySelector(`#player-${activPlayer}`).classList.toggle('active');
+    activPlayer === 1 ? activPlayer = 2 : activPlayer = 1;
+    document.querySelector(`#player-${activPlayer}`).classList.toggle('active');
+}
+
+function init(){
+    img.classList.add('hidden');
+
+    activPlayer = 1;
+    scores =[0,0];
+    currentScore=0;
+    document.querySelector(`#player-${activPlayer}`).classList.add('active');
+    document.querySelector(`#player-${activPlayer+1}`).classList.remove('active');
+    document.querySelector(`#pl-${activPlayer}-point`).innerHTML = 0; 
+    document.querySelector(`#pl-${activPlayer+1}-point`).innerHTML = 0; 
+    document.querySelector(`#current-pl${activPlayer}`).innerHTML = 0; 
+    document.querySelector(`#current-pl${activPlayer+1}`).innerHTML = 0; 
 }
